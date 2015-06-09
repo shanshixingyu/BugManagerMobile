5704ff70240a5d101b3ff250c36ee23aed729a80
yingfeng <h1024246550@163.com>
2015-06-09 09:04:04 +0800

    做了缺陷提交的部分除缺陷截图和福建外的缺陷内容提交界面

--- src/com/gf/BugManagerMobile/HomeActivity.java
+++ src/com/gf/BugManagerMobile/HomeActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.PopupWindow;
 import android.widget.Toast;
 import com.alibaba.fastjson.JSON;
 import com.gf.BugManagerMobile.adapter.HomeBugOverLvAdapter;
